645a0857929e3ae57fbc36b81e964c9918e5e451
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-06-20 18:58:21 +0200

    init slider

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
+const slides = document.querySelectorAll('.carrousel__item');
 
+function slideImages(){
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.add('slide');
+  }
+}
+
+slideImages();
 console.log('>> Ready :)');
