29f723f9da3847046bf56c62f34d4b16c9499a4b
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-06-20 21:35:00 +0200

    first try carousel

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,11 +1,26 @@
 'use strict';
-const slides = document.querySelectorAll('.carrousel__item');
+const carouselSlide = document.querySelector('.carrousel__container');
+const carouselItem = document.querySelectorAll('.carrousel__item');
 
-function slideImages(){
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.add('slide');
-  }
+const next = document.querySelector('.two');
+const prev = document.querySelector('.one');
+
+let counter = 0;
+const size = carouselItem[0].clientWidth;
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+next.addEventListener('click', () =>{
+  carouselSlide.style.transition = 'transform 0.4 ease-in-out';
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 }
+);
 
-slideImages();
-console.log('>> Ready :)');
+prev.addEventListener('click', () =>{
+  carouselSlide.style.transition = 'transform 0.4 ease-in-out';
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}
+);
+
+
