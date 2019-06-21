11acbea56e7c88c833b620ac3e091840b11ad634
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-06-21 18:25:00 +0200

    docs

---
+++ docs/assets/js/main.js
@@ -0,0 +1,1 @@
+"use strict";$(function(){$(".js__slider").slick({arrows:!1,dots:!0,centerMode:!0,variableWidth:!0,infinite:!1,slidesToShow:1,mobileFirst:!0,responsive:[{breakpoint:767,settings:{centerMode:!1,slidesToShow:2,slidesToScroll:1,variableWidth:!1}},{breakpoint:1280,settings:"unslick"}]})});
