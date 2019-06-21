2f52964a7965d26fd0c8715864897e7077a6c533
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-06-21 10:56:17 +0200

    try add slick

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,26 +1,6 @@
-'use strict';
-const carouselSlide = document.querySelector('.carrousel__container');
-const carouselItem = document.querySelectorAll('.carrousel__item');
-
-const next = document.querySelector('.two');
-const prev = document.querySelector('.one');
-
-let counter = 0;
-const size = carouselItem[0].clientWidth;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-next.addEventListener('click', () =>{
-  carouselSlide.style.transition = 'transform 0.4 ease-in-out';
-  counter++;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}
-);
-
-prev.addEventListener('click', () =>{
-  carouselSlide.style.transition = 'transform 0.4 ease-in-out';
-  counter--;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}
-);
-
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
 
